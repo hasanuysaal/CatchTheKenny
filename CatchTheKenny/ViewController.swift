d6dd0c2be841bef611c0e204af7ece021b66da60
hasanuysaal <hsnuysaal@icloud.com>
2022-10-07 19:12:03 +0300

    created a timer to change kenny points on the screen, created CGPoint array to navigate kenny's location randomly on the screen.

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -15,21 +15,34 @@ class ViewController: UIViewController {
     let scoreLabel = UILabel()
     let hScoreLabel = UILabel()
     var timer = Timer()
+    var kennyTimer = Timer()
     var score = 0
     var hScore = 0
     var counter = 10
+    var kennyPoints = [CGPoint]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let width = view.frame.size.width
         let height = view.frame.size.height
-        
+        kennyPoints = [
+            CGPoint(x: width * 0.20, y: height * 0.35),
+            CGPoint(x: width * 0.50, y: height * 0.35),
+            CGPoint(x: width * 0.80, y: height * 0.35),
+            CGPoint(x: width * 0.20, y: height * 0.59),
+            CGPoint(x: width * 0.50, y: height * 0.59),
+            CGPoint(x: width * 0.80, y: height * 0.59),
+            CGPoint(x: width * 0.20, y: height * 0.73),
+            CGPoint(x: width * 0.50, y: height * 0.73),
+            CGPoint(x: width * 0.80, y: height * 0.73)
+            
+        ]
         uiimageView.contentMode = UIView.ContentMode.scaleToFill
         uiimageView.frame.size.width = width * 0.25
         uiimageView.frame.size.height = height * 0.15
-        uiimageView.center = CGPoint(x: width * 0.25, y: height * 0.35)
         uiimageView.image = kennyImage
+        uiimageView.isHidden = true
         
         uiimageView.isUserInteractionEnabled = true
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapTheKenny))
@@ -45,7 +58,7 @@ class ViewController: UIViewController {
         
         scoreLabel.text = "Score : \(score)"
         scoreLabel.textAlignment = .center
-        scoreLabel.frame = CGRect(x: width * 0.5 - ((width-50)/2), y: height * 0.15 + 10, width: width - 50, height: height * 0.05)
+        scoreLabel.frame = CGRect(x: width * 0.5 - ((width-50)/2), y: height * 0.17, width: width - 50, height: height * 0.05)
         scoreLabel.textColor = .white
         scoreLabel.backgroundColor = .orange
         scoreLabel.shadowColor = .black
@@ -61,10 +74,10 @@ class ViewController: UIViewController {
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
         
+        kennyTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(kennyTimerFunc), userInfo: nil, repeats: true)
+        
         addViews(views: [uiimageView, timerLabel, scoreLabel, hScoreLabel])
-        
-        
-        
+  
     }
     
     func addViews(views: [UIView]){
@@ -81,14 +94,29 @@ class ViewController: UIViewController {
         
     }
     
+    @objc func kennyTimerFunc(){
+        
+       
+        let random = Int(arc4random_uniform(UInt32(self.kennyPoints.count - 1)))
+        self.uiimageView.center = self.kennyPoints[random]
+        self.uiimageView.isHidden = false
+        
+        
+    }
+    
     @objc func timerFunc(){
         
-        if counter == 1 {
-            timer.invalidate()
+        self.counter -= 1
+        self.timerLabel.text = "Time Left : \(self.counter)"
+        
+        if counter == 0 {
+            
+            self.uiimageView.isHidden = true
+            self.timer.invalidate()
+            self.kennyTimer.invalidate()
         }
         
-        self.counter -= 1
-        timerLabel.text = "Time Left : \(counter)"
+        
     }
 
 }
